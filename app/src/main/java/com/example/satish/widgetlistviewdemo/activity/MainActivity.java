3de194bbaa073a65a9baf20bfa7cd464b9088c60
Lincoln <Lincoln@apples-MacBook-Air-2.local>
2015-12-02 13:04:25 +0530

    added list scroll

--- app/src/main/java/com/example/satish/widgetlistviewdemo/activity/MainActivity.java
+++ app/src/main/java/com/example/satish/widgetlistviewdemo/activity/MainActivity.java
@@ -1,31 +1,33 @@
 package com.example.satish.widgetlistviewdemo.activity;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.example.satish.widgetlistviewdemo.R;
+import com.example.satish.widgetlistviewdemo.adapter.DynamicListView;
 import com.example.satish.widgetlistviewdemo.adapter.TimeLineAdapter;
 import com.example.satish.widgetlistviewdemo.config.AppConfig;
-import com.example.satish.widgetlistviewdemo.helper.ListViewFactory;
 import com.example.satish.widgetlistviewdemo.model.TimeLine;
-import com.example.satish.widgetlistviewdemo.service.WidgetService;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements TimeLineAdapter.ListDragDropListener {
-    private ListView listView;
-    public  static ArrayList<TimeLine> timeLineArrayList;
-    private TimeLineAdapter timeLineAdapter;
-    private final String TAG = MainActivity.class.getSimpleName();
+public class MainActivity extends AppCompatActivity implements DynamicListView.DynamicListViewListener, AdapterView.OnItemClickListener {
+
+    private String TAG = MainActivity.class.getSimpleName();
+    private DynamicListView listView;
+    public static ArrayList<TimeLine> timeLineArrayList;
     private EditText editText;
     private TextView lblTime;
     private Button button;
@@ -33,26 +35,27 @@ public class MainActivity extends AppCompatActivity implements TimeLineAdapter.L
     private RelativeLayout relativeLayout;
     private String et_task;
     private int position_value;
-    private WidgetService widgetService;
-    private ListViewFactory listViewFactory;
+    private TimeLineAdapter mAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        listView = (ListView) findViewById(R.id.list);
+
+        listView = (DynamicListView) findViewById(R.id.listview);
         editText = (EditText) findViewById(R.id.et_text);
         button = (Button) findViewById(R.id.btn_add);
-        lblTime= (TextView) findViewById(R.id.edt_lbl_time);
+        lblTime = (TextView) findViewById(R.id.edt_lbl_time);
         relativeLayout = (RelativeLayout) findViewById(R.id.addTagLayout);
-        imageButton= (ImageButton) findViewById(R.id.btn_mic);
+        imageButton = (ImageButton) findViewById(R.id.btn_mic);
         //event on voice image button
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i=new Intent(getApplicationContext(),ToDoNoteVoiceActivity.class);
-                TimeLine timeLine=timeLineArrayList.get(position_value);
-                i.putExtra("time",timeLine.getTime());
-                i.putExtra("task",timeLine.getTask());
+                Intent i = new Intent(getApplicationContext(), ToDoNoteVoiceActivity.class);
+                TimeLine timeLine = timeLineArrayList.get(position_value);
+                i.putExtra("time", timeLine.getTime());
+                i.putExtra("task", timeLine.getTask());
                 startActivity(i);
             }
         });
@@ -63,57 +66,78 @@ public class MainActivity extends AppCompatActivity implements TimeLineAdapter.L
                 et_task = editText.getText().toString();
                 TimeLine timeLine = timeLineArrayList.get(position_value);
                 timeLine.setTask(et_task);
-                timeLineArrayList.remove(position_value);
-                timeLineArrayList.add(position_value, timeLine);
-                timeLineAdapter.notifyDataSetChanged();
+                timeLineArrayList.set(position_value, timeLine);
+                mAdapter.notifyDataSetChanged();
                 editText.setText("");
                 relativeLayout.setVisibility(View.GONE);
+
+                // hide the keyboard
+                hideSoftKeyboard();
             }
         });
-        Log.d(TAG,"main activity");
-        //adding timeline objects to timelinearraylist
+
         timeLineArrayList = new ArrayList<>();
         for (int i = 0; i < AppConfig.task.length; i++) {
             TimeLine timeLine = new TimeLine(AppConfig.task[i], AppConfig.time[i]);
             timeLineArrayList.add(timeLine);
         }
-        timeLineAdapter = new TimeLineAdapter(this, timeLineArrayList);
-        //set adapter to listview
-        listView.setAdapter(timeLineAdapter);
-        //
-        timeLineAdapter.setListDragDropListener(this);
+
+        mAdapter = new TimeLineAdapter(this, timeLineArrayList);
+
+        listView.setDynamicListViewListener(this);
+        listView.setCheeseList(timeLineArrayList);
+        listView.setAdapter(mAdapter);
+        //listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+
+        listView.setOnItemClickListener(this);
+    }
+
+
+    @Override
+    public void onRowSwap(int fromPosition, int toPosition) {
+        Log.e(TAG, "onRowSwap: " + fromPosition + ", " + toPosition);
+
+        TimeLine fromTask = timeLineArrayList.get(fromPosition);
+
+        TimeLine toTask = timeLineArrayList.get(toPosition);
+        toTask.setTask(fromTask.getTask());
+
+        fromTask.setTask("");
+
+        timeLineArrayList.set(fromPosition, fromTask);
+        timeLineArrayList.set(toPosition, toTask);
+        mAdapter.notifyDataSetChanged();
     }
 
     @Override
-    public void onDropListItem(int position, String task) {
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
         TimeLine timeLine = timeLineArrayList.get(position);
-        timeLine.setTask(task);
-        timeLineArrayList.remove(position);
-        timeLineArrayList.add(position, timeLine);
-        timeLineAdapter.notifyDataSetChanged();
-    }
-
-    @Override
-    public void onItemClickListener(int position) {
-        TimeLine timeLine=timeLineArrayList.get(position);
         lblTime.setText(timeLine.getTime());
-       //check weather listview visible or not
-        if(relativeLayout.getVisibility()!=View.VISIBLE) {
+        //check weather list view visible or not
+        if (relativeLayout.getVisibility() != View.VISIBLE) {
             relativeLayout.setVisibility(View.VISIBLE);
-        }
-        else
+
+            // show keyboard
+            if(editText.requestFocus()){
+                showSoftKeyboard();
+            }
+
+        } else {
             relativeLayout.setVisibility(View.GONE);
+            hideSoftKeyboard();
+        }
+
         position_value = position;
     }
 
-    @Override
-    public void onItemLongPressListener(int position) {
-
-        TimeLine timeLine = timeLineArrayList.get(position);
-        timeLine.setTask("");
-        timeLineArrayList.remove(position);
-        timeLineArrayList.add(position, timeLine);
-        timeLineAdapter.notifyDataSetChanged();
+    public void hideSoftKeyboard() {
+        InputMethodManager inputMethodManager = (InputMethodManager) getApplicationContext().getSystemService(Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
     }
 
-   }
+    public void showSoftKeyboard(){
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED,0);
+    }
+}
--- app/src/main/java/com/example/satish/widgetlistviewdemo/adapter/TimeLineAdapter.java
+++ app/src/main/java/com/example/satish/widgetlistviewdemo/adapter/TimeLineAdapter.java
@@ -1,184 +1,82 @@
 package com.example.satish.widgetlistviewdemo.adapter;
 
-import android.annotation.TargetApi;
-import android.app.Activity;
-import android.content.ClipData;
-import android.content.ClipDescription;
 import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Point;
-import android.graphics.drawable.ColorDrawable;
-import android.os.Build;
-import android.util.Log;
-import android.view.DragEvent;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.ToggleButton;
 
 import com.example.satish.widgetlistviewdemo.R;
-import com.example.satish.widgetlistviewdemo.activity.MainActivity;
 import com.example.satish.widgetlistviewdemo.model.TimeLine;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 
 /**
  * Created by Satish on 09-11-2015.
  */
 public class TimeLineAdapter extends BaseAdapter {
-    private ListDragDropListener listDragDropListener;
-    private Activity activity;
-    private LayoutInflater inflater;
+
+    private String TAG  = TimeLineAdapter.class.getSimpleName();
+    private Context mContext;
+    final int INVALID_ID = -1;
     private ArrayList<TimeLine> timeLineArrayList;
-    private String data_task;
-    private int itemLongPressPosition;
 
+    HashMap<Integer, Integer> mIdMap = new HashMap<Integer, Integer>();
 
-    public TimeLineAdapter(Activity activity, ArrayList<TimeLine> timeLineArrayList) {
-        this.activity = activity;
+    public TimeLineAdapter(Context context, ArrayList<TimeLine> timeLineArrayList) {
+        super();
+        mContext = context;
         this.timeLineArrayList = timeLineArrayList;
 
-    }
+        for (int i = 0; i < timeLineArrayList.size(); ++i) {
+            mIdMap.put(i, i);
+        }
 
-    public void setListDragDropListener(MainActivity listDragDropListener) {
-        this.listDragDropListener = listDragDropListener;
-    }
-
-    @Override
-    public int getCount() {
-        return timeLineArrayList.size();
-    }
-
-    @Override
-    public Object getItem(int position) {
-        return timeLineArrayList.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return position;
+
+        if (position < 0 || position >= mIdMap.size()) {
+            return INVALID_ID;
+        }
+        String item = getItem(position);
+        //Log.e(TAG, "getItemId: " + position + ", item: " + item);
+        return mIdMap.get(position);
     }
 
-    @TargetApi(Build.VERSION_CODES.M)
     @Override
-    public View getView(final int position, View convertView, ViewGroup parent) {
-        View view = convertView;
-        if (view == null)
-            inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        if (convertView == null)
-            view = inflater.inflate(R.layout.list_item, null);
+    public boolean hasStableIds() {
+        return true;
+    }
 
-        final ToggleButton imgbtnCheck = (ToggleButton) view.findViewById(R.id.img_check);
-        imgbtnCheck.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // TODO: ON CHECKBOX BUTTON ACTIONS
-            }
-        });
 
-        final TextView lblTask = (TextView) view.findViewById(R.id.txt_task);
-        final TextView lblTime = (TextView) view.findViewById(R.id.txt_time);
-        final TimeLine timeLine = timeLineArrayList.get(position);
+    @Override
+    public int getCount() {
+        return mIdMap.size();
+    }
 
-        lblTime.setText(timeLine.getTime());
-        lblTask.setText(timeLine.getTask());
-        view.setOnScrollChangeListener(new View.OnScrollChangeListener() {
-            @Override
-            public void onScrollChange(View v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
+    // getView method is called for each item of ListView
+    public View getView(int position, View view, ViewGroup parent) {
+        // inflate the layout for each item of listView
+        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        view = inflater.inflate(R.layout.list_item, null);
 
-            }
-        });
-        //list item long click action
-        view.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                itemLongPressPosition = position;
-                TimeLine timeLine = timeLineArrayList.get(position);
-                if (timeLine.getTask().length() != 0) {
-                    ClipData.Item item = new ClipData.Item(timeLine.getTask());
-                    String[] mimeTypes = {ClipDescription.MIMETYPE_TEXT_PLAIN};
-                    data_task = (String) item.getText();
-                    ClipData data = new ClipData(timeLine.getTask(), mimeTypes, item);
-                    View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(v);
-                    v.startDrag(data, //data to be dragged
-                            shadowBuilder, //drag shadow
-                            v, //local data about the drag and drop operation
-                            0   //no needed flags
-                    );
+        // get the reference of textViews
+        TextView textViewConatctNumber = (TextView) view.findViewById(R.id.txt_task);
+        TextView txtTime = (TextView) view.findViewById(R.id.txt_time);
 
-                    return true;
-                } else return false;
-            }
-        });
-        //list item click action
-        view.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                listDragDropListener.onItemClickListener(position);
-            }
-        });
-        //list item drag action
-        view.setOnDragListener(new View.OnDragListener() {
-            @Override
-            public boolean onDrag(View v, DragEvent event) {
-                switch (event.getAction()) {
-                    case DragEvent.ACTION_DRAG_STARTED:
-                        break;
-                    case DragEvent.ACTION_DRAG_ENTERED:
-                        lblTask.setTextSize(25);//set zoom to item
-                        lblTime.setTextSize(25);
-                        v.setBackgroundColor(activity.getResources().getColor(R.color.colorSelect));//set background color to list item
-                        break;
-                    case DragEvent.ACTION_DRAG_EXITED:
-                        Log.i("Drag ", "exited");   //change the shape of the view back to normal
-                        lblTask.setTextSize(15);    //set original size to list item
-                        lblTime.setTextSize(15);
-                        v.setBackgroundColor(activity.getResources().getColor(R.color.colorWhite));
-                        break;
-                    case DragEvent.ACTION_DROP:
-                        Log.i("Drag ", "drop");
-                        lblTask.setTextSize(15);
-                        lblTime.setTextSize(15);
-                        listDragDropListener.onDropListItem(position, data_task);//here pass the item position and task value
-                        listDragDropListener.onItemLongPressListener(itemLongPressPosition);
-                        v.setBackgroundColor(activity.getResources().getColor(R.color.colorWhite));
-                        break;
-                    case DragEvent.ACTION_DRAG_ENDED:
-                        Log.i("Drag ", "drag ended");  //go back to normal shape/**/
-                    default:
-                        break;
-                }
-                return true;
-            }
-        });
-        view.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                return false;
-            }
-        });
+        // Set the Sender number and smsBody to respective TextViews
+        textViewConatctNumber.setText(timeLineArrayList.get(position).getTask());
+        txtTime.setText(timeLineArrayList.get(position).getTime());
+
         return view;
     }
 
-
-    @Override
-    public int getViewTypeCount() {
-        return (1);
-    }
-
-    public interface ListDragDropListener {
-
-        void onDropListItem(int position, String task);
-
-        void onItemClickListener(int position);
-
-        void onItemLongPressListener(int position);
-
-
+    public String getItem(int position) {
+        return timeLineArrayList.get(position).getTask();
     }
 }
